53d9e4a5549bd16bcef8c8fe0c5e0c23d40b8061
skyllic <skyllic@gmail.com>
2022-01-21 13:16:18 +1030

    Re-implemented Dilemma 2 nerf
    Re-implemented Dilemma 2 nerf on MoI difficulty.

--- T9-DLC MoI Difficulty.js
+++ T9-DLC MoI Difficulty.js
@@ -382,7 +382,7 @@ var init = () => {
         switch(level+1)
         {
             case 1: cost = 1e10; break;
-            case 2: cost = 3.85e1; break;
+            case 2: cost = 3.85e22; break;
             case 3: cost = 1e20; break;
             case 4: cost = 1.0001e10; break; // To compensate for numerical errors
             case 5: cost = 1e25; break;
